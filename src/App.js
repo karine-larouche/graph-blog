581b71b223af922005eb87a745509db57e321245
Karine Larouche <karine.larouche14@gmail.com>
2020-01-11 09:59:48 -0500

    Add linter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+const App = () => <div className="App" />;
 
 export default App;
