5f5bf0e202ceb683f85397fb3374222fc8eb1fd9
Karine Larouche <karine.larouche14@gmail.com>
2020-01-12 16:15:17 -0500

    Display pie chart for owned games plays

--- src/pages/Bgg/PlaysForOwnedGames.js
+++ src/pages/Bgg/PlaysForOwnedGames.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Pie } from '@vx/shape';
+import { Group } from '@vx/group';
+import { red, orange, amber, lightGreen } from '@material-ui/core/colors';
 import { sortBy } from '../../utils/arrayUtils';
 
-const playGroupLabels = [
-  'Never played',
-  '1 play',
-  '2 plays',
-  '3-4 plays',
-  '5-9 plays',
-  '10-25 plays',
-  '25+ plays',
+const emptyPlayGroups = () => [
+  { label: 'Never played', color: red[700], games: [] },
+  { label: '1 play', color: orange[800], games: [] },
+  { label: '2 plays', color: amber[500], games: [] },
+  { label: '3-4 plays', color: lightGreen[600], games: [] },
+  { label: '5-9 plays', color: lightGreen[700], games: [] },
+  { label: '10-25 plays', color: lightGreen[800], games: [] },
+  { label: '25+ plays', color: lightGreen[900], games: [] },
 ];
 
 const playGroupIndex = numPlays => {
@@ -21,7 +24,7 @@ const playGroupIndex = numPlays => {
 };
 
 const groupByPlays = games => {
-  const grouped = playGroupLabels.map(label => ({ label, games: [] }));
+  const grouped = emptyPlayGroups();
   sortBy(games, 'numPlays').forEach(game => {
     grouped[playGroupIndex(game.numPlays)].games.push(game);
   });
@@ -33,20 +36,49 @@ const PlaysForOwnedGames = ({ isFetching, hasError, games, username }) => {
   if (hasError) return 'An error occured... sorry!';
   if (!games) return 'Enter your bgg username to view your plays.';
   if (games.length === 0)
-    return "You don't have any logged plays! Log your plays on bgg to see your progress.";
+    return 'Mark some games as owned on bgg to see this chart.';
+
+  const playAmounts = groupByPlays(games);
+  const noRecordedPlays = !playAmounts.slice(1).find(a => a.games.length);
+  if (noRecordedPlays) return 'Log your plays on bgg to see this chart.';
+
+  const [width, height] = [220, 220];
+  const radius = Math.min(width, height) / 2;
+  const centerX = width / 2;
+  const centerY = height / 2;
   return (
-    <ul>
-      {groupByPlays(games).map(group => (
-        <li>
-          {group.label}
-          <ul>
-            {group.games.map(game => (
-              <li>{`${game.numPlays} - ${game.name}`}</li>
-            ))}
-          </ul>
-        </li>
-      ))}
-    </ul>
+    <>
+      <svg width={width} height={height}>
+        <Group top={centerY} left={centerX}>
+          <Pie
+            data={playAmounts}
+            pieValue={d => d.games.length}
+            pieSortValues={() => -1}
+            outerRadius={radius}
+          >
+            {pie =>
+              pie.arcs.map((arc, i) => (
+                <g key={arc.data.label}>
+                  <path d={pie.path(arc)} fill={playAmounts[i].color} />
+                </g>
+              ))
+            }
+          </Pie>
+        </Group>
+      </svg>
+      <ul>
+        {groupByPlays(games).map(group => (
+          <li key={group.label}>
+            {group.label}
+            <ul>
+              {group.games.map(game => (
+                <li key={game.name}>{`${game.numPlays} - ${game.name}`}</li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
