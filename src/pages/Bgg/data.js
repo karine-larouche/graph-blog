cbaaec5c19edb18b192ea02f07ef4758d4a8e9f8
Karine Larouche <karine.larouche14@gmail.com>
2020-02-01 12:30:21 -0500

    Fetch all pages when fetching plays

--- src/pages/Bgg/data.js
+++ src/pages/Bgg/data.js
@@ -1,10 +1,11 @@
+import { range } from '../../utils/arrayUtils';
 import fetchWithRetry from '../../utils/fetchWithRetry';
 
 const ownedGamesUrl = username =>
   `https://www.boardgamegeek.com/xmlapi2/collection?username=${username}&own=1&excludesubtype=boardgameexpansion`;
 
-const playsUrl = username =>
-  `https://www.boardgamegeek.com/xmlapi2/plays?username=${username}`;
+const playsUrl = (username, page = 1) =>
+  `https://www.boardgamegeek.com/xmlapi2/plays?username=${username}&page=${page}`;
 
 const parseOwnedGames = data => {
   const document = new DOMParser().parseFromString(data, 'text/xml');
@@ -21,7 +22,7 @@ const isNotExpansion = play =>
     s => s.getAttribute('value') === 'boardgameexpansion',
   );
 
-const parsePlays = async data => {
+const parsePlays = data => {
   const document = new DOMParser().parseFromString(data, 'text/xml');
   return [...document.getElementsByTagName('play')]
     .filter(isNotExpansion)
@@ -39,7 +40,28 @@ export const getOwnedGames = async username => {
     : undefined;
 };
 
+const totalPlayPages = data => {
+  const document = new DOMParser().parseFromString(data, 'text/xml');
+  return Math.ceil(
+    document.getElementsByTagName('plays')[0].getAttribute('total') / 100,
+  );
+};
+
 export const getPlayedGames = async username => {
   const result = await fetchWithRetry(playsUrl(username));
-  return result && result.status === 200 ? parsePlays(result.data) : undefined;
+  if (!result || result.status !== 200) return undefined;
+
+  const plays = parsePlays(result.data);
+
+  const playPages = await Promise.all(
+    range(2, totalPlayPages(result.data)).map(async page => {
+      const pageResult = await fetchWithRetry(playsUrl(username, page));
+      return pageResult && pageResult.status === 200
+        ? parsePlays(pageResult.data)
+        : undefined;
+    }),
+  );
+  if (playPages.find(p => p === undefined)) return undefined;
+
+  return plays.concat(...playPages);
 };
--- src/utils/arrayUtils.js
+++ src/utils/arrayUtils.js
@@ -1,2 +1,4 @@
-// eslint-disable-next-line import/prefer-default-export
+export const range = (f, l) =>
+  Array.from({ length: l - f + 1 }, (v, i) => i + f);
+
 export const sortBy = (array, key) => array.sort((a, b) => b[key] - a[key]);
