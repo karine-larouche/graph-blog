1c3bb508605128fb43ff8aaea5ee19edbbd52eda
Karine Larouche <karine.larouche14@gmail.com>
2020-03-07 10:16:23 -0500

    Fix: Use alphabetical sort for strings

--- src/utils/arrayUtils.js
+++ src/utils/arrayUtils.js
@@ -3,7 +3,11 @@ export const last = array => array[array.length - 1];
 export const range = (f, l) =>
   Array.from({ length: l - f + 1 }, (v, i) => i + f);
 
-export const sort = array => array.sort((a, b) => a - b);
+export const sort = array =>
+  array.length > 1 && !Number.isNaN(array[0] - array[1])
+    ? array.sort((a, b) => a - b)
+    : array.sort();
+
 export const sortBy = (array, key) => array.sort((a, b) => b[key] - a[key]);
 
 export const min = array => sort(array)[0];
