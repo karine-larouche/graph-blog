d8fffe96e37d8f784a8190bbe53c4e321c34f585
Karine Larouche <karine.larouche14@gmail.com>
2020-03-29 09:13:48 -0400

    Add theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
 import Layout from './pages/Layout';
 
 const App = () => (
   <Router>
-    <Layout />
+    <ThemeProvider theme={theme}>
+      <Layout />
+    </ThemeProvider>
   </Router>
 );
 
---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { pink } from '@material-ui/core/colors';
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: pink[900],
+    },
+  },
+});
