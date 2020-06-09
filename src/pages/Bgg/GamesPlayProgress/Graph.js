5790953e484bf9113b9cd9b0e7fda3f167e2e051
Karine Larouche <karine.larouche14@gmail.com>
2020-06-09 11:02:46 -0400

    Add x-axis to play progress by game

--- src/pages/Bgg/GamesPlayProgress/Graph.js
+++ src/pages/Bgg/GamesPlayProgress/Graph.js
@@ -1,16 +1,33 @@
 import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
 import { grey } from '@material-ui/core/colors';
 import { Group } from '@vx/group';
 import { GlyphDot } from '@vx/glyph';
 import { LinePath } from '@vx/shape';
 import { scaleTime, scaleLinear } from '@vx/scale';
 import { curveMonotoneX } from '@vx/curve';
-import { subDays } from 'date-fns';
-import { last, min, max } from '../../../utils/arrayUtils';
+import { AxisBottom } from '@vx/axis';
+import {
+  subDays,
+  differenceInCalendarMonths,
+  isSameMonth,
+  format,
+  isAfter,
+  isBefore,
+} from 'date-fns';
+import { last, min, max, range } from '../../../utils/arrayUtils';
 import { gameWithPlayProgressShape } from './utils';
 
-const borderWidth = 4;
+const scalePadding = 16;
+const horizontalPadding = 8;
+
+const useStyles = makeStyles({
+  tickLines: {
+    strokeWidth: 2,
+    transform: 'translate(0, 1px)',
+  },
+});
 
 /* eslint-disable react/prop-types */
 const SingleLine = React.memo(
@@ -40,21 +57,51 @@ const GamesPlayProgressGraph = ({
   width,
   height,
 }) => {
-  const [x, y] = useMemo(() => {
+  const classes = useStyles();
+
+  const [x, y, xScale, start, end, graphPosition] = useMemo(() => {
+    const position = {
+      top: 1,
+      bottom: height - scalePadding,
+      left: horizontalPadding,
+      right: width - horizontalPadding,
+    };
+
     const earliestPlay = min(playsPerGame.map(game => game.totalPlays[0].date));
     const startDate = subDays(new Date(earliestPlay), 1);
-    const xScale = scaleTime({ domain: [startDate, new Date()] });
-    xScale.range([borderWidth, width - borderWidth]);
+    const xAxisScale = scaleTime({
+      domain: [startDate, new Date()],
+      range: [position.left, position.right],
+    });
 
     const maxPlays = max(playsPerGame.map(game => last(game.totalPlays).total));
-    const yScale = scaleLinear({ domain: [0, maxPlays] });
-    yScale.range([height - borderWidth, borderWidth]);
+    const yScale = scaleLinear({
+      domain: [0, maxPlays],
+      range: [position.bottom, position.top],
+    });
 
-    return [d => xScale(new Date(d.date)), d => yScale(d.total)];
+    return [
+      d => xScale(new Date(d.date)),
+      d => yScale(d.total),
+      xAxisScale,
+      startDate,
+      new Date(),
+      position,
+    ];
   }, [playsPerGame, width, height]);
 
   const isHighlighted = game =>
     Boolean(highlightedGame) && highlightedGame.name === game.name;
+
+  const showXAxis = differenceInCalendarMonths(end, start) >= 12;
+  const years = range(start.getFullYear(), end.getFullYear());
+  const isWithinRange = d => isAfter(d, start) && isBefore(d, end);
+  const tickValues = years
+    .map(year => new Date(`${year}-01-01T00:00`))
+    .filter(isWithinRange);
+  const tickLabelValues = years
+    .map(year => new Date(`${year}-07-01T00:00`))
+    .filter(isWithinRange);
 
   return (
     <svg viewBox={`0 0 ${width} ${height}`} height={`${height}px`}>
@@ -86,17 +133,41 @@ const GamesPlayProgressGraph = ({
             </g>
           ))}
       </Group>
-      <rect
-        x={borderWidth / 2}
-        y={borderWidth / 2}
-        width={width - borderWidth}
-        height={height - borderWidth}
-        fill="none"
-        stroke={grey[300]}
-        strokeWidth={borderWidth}
-        rx={12}
-        ry={12}
-      />
+      {showXAxis && (
+        <>
+          <AxisBottom
+            top={graphPosition.bottom}
+            scale={xScale}
+            tickValues={tickValues}
+            tickFormat={() => ''}
+            tickStroke={grey[800]}
+            stroke={grey[800]}
+            strokeWidth={2}
+            tickLength={14}
+            tickClassName={classes.tickLines}
+          />
+          <AxisBottom
+            top={graphPosition.bottom - 4}
+            scale={xScale}
+            tickValues={tickLabelValues}
+            tickFormat={v => format(v, 'yyyy')}
+            tickLabelProps={value => ({
+              fontWeight: 500,
+              fill: grey[800],
+              textAnchor:
+                // eslint-disable-next-line no-nested-ternary
+                differenceInCalendarMonths(value, start) <= 1
+                  ? 'start'
+                  : isSameMonth(value, end)
+                  ? 'end'
+                  : 'middle',
+            })}
+            hideAxisLine
+            hideTicks
+            tickClassName="tickLabels"
+          />
+        </>
+      )}
     </svg>
   );
 };
