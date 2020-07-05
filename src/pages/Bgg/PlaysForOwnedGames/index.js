b16c1edc2e7a08c15b13870ce82db9f19b5e331e
Karine Larouche <karine.larouche14@gmail.com>
2020-07-05 11:38:29 -0400

    Exclude non-boardgame items and multiple version of same item

--- src/pages/Bgg/PlaysForOwnedGames/index.js
+++ src/pages/Bgg/PlaysForOwnedGames/index.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { red, orange, amber, lightGreen } from '@material-ui/core/colors';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import { sortBy } from '../../../utils/arrayUtils';
 import BggInstructions from '../../../components/BggInstruction';
 import Legend from '../../../components/Legend';
 import Pie from './Pie';
@@ -29,7 +28,7 @@ const playGroupIndex = numPlays => {
 
 const groupByPlays = games => {
   const grouped = emptyPlayGroups();
-  sortBy(games, 'numPlays').forEach(game => {
+  games.forEach(game => {
     grouped[playGroupIndex(game.numPlays)].games.push(game);
   });
   return grouped;
--- src/pages/Bgg/data/ownedGames.js
+++ src/pages/Bgg/data/ownedGames.js
@@ -1,19 +1,41 @@
 import fetchWithRetry from '../../../utils/fetchWithRetry';
+import { sortBy } from '../../../utils/arrayUtils';
+
+const excludedObjectIds = [
+  '33456', // Miscellaneous Card Game Accessory
+  '5985', // Miscellaneous Game Accessory
+  '22427', // Miscellaneous Game Book
+  '96628', // Miscellaneous Game Compilation
+  '18785', // Miscellaneous Game Magazine
+  '22104', // Miscellaneous Game Merchandise
+  '31693', // Miscellaneous Miniatures Game Accessory
+  '23953', // Outside the Scope of BGG
+];
 
 const ownedGamesUrl = username =>
   `https://www.boardgamegeek.com/xmlapi2/collection?username=${username}&own=1&excludesubtype=boardgameexpansion`;
 
+const sortAndRemoveDuplicates = games =>
+  sortBy(games, 'numPlays').filter(
+    (game, i, sortedGames) => i === 0 || game.name !== sortedGames[i - 1].name,
+  );
+
 const parseOwnedGames = data => {
   const document = new DOMParser().parseFromString(data, 'text/xml');
-  return [...document.getElementsByTagName('item')].map(i => ({
-    name: i.getElementsByTagName('name')[0].childNodes[0].nodeValue,
-    numPlays: parseInt(
-      i.getElementsByTagName('numplays')[0].childNodes[0].nodeValue,
-    ),
-  }));
+  return [...document.getElementsByTagName('item')]
+    .filter(i => !excludedObjectIds.includes(i.getAttribute('objectid')))
+    .map(i => ({
+      name:
+        i.getElementsByTagName('originalname').length > 0
+          ? i.getElementsByTagName('originalname')[0].childNodes[0].nodeValue
+          : i.getElementsByTagName('name')[0].childNodes[0].nodeValue,
+      numPlays: parseInt(
+        i.getElementsByTagName('numplays')[0].childNodes[0].nodeValue,
+      ),
+    }));
 };
 
 export default async (username, errorState) => {
   const result = await fetchWithRetry(ownedGamesUrl(username), errorState);
-  return result && parseOwnedGames(result.data);
+  return result && sortAndRemoveDuplicates(parseOwnedGames(result.data));
 };
