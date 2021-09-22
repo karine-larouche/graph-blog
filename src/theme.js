30df4a28f85597388811a2fe5d7d9d5f188bbac7
Karine Larouche <karine.larouche14@gmail.com>
2021-09-22 10:24:44 -0400

    Upgrade dependencies

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,7 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/core/styles';
 import { pink } from '@material-ui/core/colors';
 
-export default createMuiTheme({
+export default createTheme({
   palette: {
     primary: {
       main: pink[900],
